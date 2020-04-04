0ca55c93fa05ec5db1d4e434f583ccf76a6ed088
Pedro Lemos <lemozxpedro@gmail.com>
2020-04-04 19:03:37 -0300

    Finished the application

---
+++ README.md
@@ -0,0 +1,9 @@
+# XylophoneApp
+
+An application that play Xylophone sound.
+I learned how to use functions to build Widgets with this application.
+
+## Screenshot
+
+<p>
+    <img src="https://i.imgur.com/3jZIRnD.png">
--- lib/main.dart
+++ lib/main.dart
@@ -7,47 +7,52 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: HomeScreen(),
       ),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
+class HomeScreen extends StatelessWidget {
+  // We are using a function to play the desired sound
+  void playSound(soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
 
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            Tile(),
-            Tile(),
-            Tile(),
-            Tile(),
-            Tile(),
-            Tile(),
-          ],
-        ),
+  // Instead of repeating a lot of code, we can create a function
+  // to export a widget, accepting certain parameters
+  Expanded createTile({Color color, int soundNumber}) {
+    // Named variables should always be enclosed by curly braces
+    return Expanded(
+      child: FlatButton(
+        child: null,
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
       ),
     );
   }
-}
 
-class Tile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FlatButton(
-      onPressed: (){
-        final player = AudioCache();
-        player.play('note1.wav');
-      },
-      color: Colors.red,
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          createTile(color: Colors.red, soundNumber: 1),
+          createTile(color: Colors.orange, soundNumber: 2),
+          createTile(color: Colors.yellow, soundNumber: 3),
+          createTile(color: Colors.green, soundNumber: 4),
+          createTile(color: Colors.teal, soundNumber: 5),
+          createTile(color: Colors.blue, soundNumber: 6),
+          createTile(color: Colors.purple, soundNumber: 7)
+        ],
+      ),
     );
   }
 }
